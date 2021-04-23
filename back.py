022fd592e881c677e1d049ff06f80fcb073e2472
Patcharapong10 <patcharapong20new@gmail.com>
2021-04-23 17:23:47 +0700

    update

--- back.py
+++ back.py
@@ -1,14 +1,68 @@
-from flask import Flask
+from flask.helpers import url_for
+import pymongo
+import bcrypt
+from flask import Flask,session,render_template,request,redirect
 from flask.templating import render_template
 import pymongo
 
 app = Flask(__name__)
 client = pymongo.MongoClient("mongodb://admin:VIDgnh48123@node12713-project.app.ruk-com.cloud:11012") 
 db = client["project"] 
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/Register")
+def Register():
+    return render_template("Register.html")
+
+########################################################################### LOGIN ###############################################################################################################
+@app.route("/login")
+def login():
+    if 'email' in session:
+        return 'You are logged in as ' + session['email']
+
+    return render_template('Login.html')
+
+@app.route('/loginBackend', methods=['POST'])
+def loginBackend():
+    users = db.customer
+    login_user = users.find_one({'email': request.form['email']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt()):
+            session['email'] = request.form['email']
+            return redirect(url_for('menu'))
+
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    char = db.customer
+    if request.method == 'POST':
+        users = char
+        existing_user = users.find_one({'username' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            emaila = request.form['email']
+            users.insert_one({'username' : request.form['username'], 'password' : hashpass , 'email':emaila})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return render_template('index.html')
+
+    return render_template('Register.html')
+
+###############################################################################################################################################################################################
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port = 3000)
